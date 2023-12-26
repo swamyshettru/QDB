01189521470fc6096a25c50cad6889713fc90b96
swamyshettru <swamy.shettru@gmail.com>
2023-12-26 12:16:14 +0530

    Delete qdb-user directory

--- qdb-user/src/Helper/Utils.ts
+++
@@ -1,5 +0,0 @@
-export const getRandomNumber =(minVal:number, maxVal: number)=>{
-    minVal= Math.ceil(minVal)
-    maxVal = Math.floor(maxVal)
-    return Math.floor(Math.random()* (maxVal-minVal)+minVal)
-}
--- qdb-user/src/components/GenericError.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const GenericError = () => {
-  return (
-    <div>Something went worong, please try again later</div>
-  )
-}
-
-export default GenericError
